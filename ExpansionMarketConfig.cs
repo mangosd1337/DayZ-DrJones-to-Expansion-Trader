2f3a1d51f83968def53fd4449aee7c0445213680
mangosd <mangosd@hotmail.de>
2021-10-27 18:29:05 +0200

    initial commit

---
+++ ExpansionMarketConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DayZ_DrJonesToExpansionMarket
+{
+    public partial class ExpansionMarketConfig
+    {
+        [JsonProperty("m_Version", NullValueHandling = NullValueHandling.Ignore)]
+        public long? MVersion { get; set; }
+
+        [JsonProperty("DisplayName", NullValueHandling = NullValueHandling.Ignore)]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("Items", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Item> Items { get; set; }
+    }
+
+    public partial class Item
+    {
+        [JsonProperty("ClassName", NullValueHandling = NullValueHandling.Ignore)]
+        public string ClassName { get; set; }
+
+        [JsonProperty("MaxPriceThreshold", NullValueHandling = NullValueHandling.Ignore)]
+        public long? MaxPriceThreshold { get; set; }
+
+        [JsonProperty("MinPriceThreshold", NullValueHandling = NullValueHandling.Ignore)]
+        public long? MinPriceThreshold { get; set; }
+
+        [JsonProperty("MaxStockThreshold", NullValueHandling = NullValueHandling.Ignore)]
+        public long? MaxStockThreshold { get; set; }
+
+        [JsonProperty("MinStockThreshold", NullValueHandling = NullValueHandling.Ignore)]
+        public long? MinStockThreshold { get; set; }
+
+        [JsonProperty("PurchaseType", NullValueHandling = NullValueHandling.Ignore)]
+        public long? PurchaseType { get; set; }
+
+        [JsonProperty("SpawnAttachments", NullValueHandling = NullValueHandling.Ignore)]
+        public List<object> SpawnAttachments { get; set; }
+
+        [JsonProperty("Variants", NullValueHandling = NullValueHandling.Ignore)]
+        public List<object> Variants { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DayZ_DrJonesToExpansionMarket
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var pathToInputFile = args[0];
+            var inputFileContent = await File.ReadAllLinesAsync(pathToInputFile);
+
+            var items = new List<Item>();
+
+            foreach (var line in inputFileContent)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                if (line.Contains("/")) continue;
+                if (line.Contains("<Trader>")) continue;
+                if (line.Contains("<Category>")) continue;
+
+                var itemValues = line.Replace("\t", string.Empty)
+                    .Replace(" ", string.Empty)
+                    .Split(",");
+
+                if (itemValues.Length != 4) continue;
+
+                items.Add(new Item
+                {
+                    ClassName = itemValues[0],
+                    MaxPriceThreshold = int.Parse(itemValues[2]),
+                    MaxStockThreshold = 100, //add config
+                    MinPriceThreshold = int.Parse(itemValues[2]),
+                    MinStockThreshold = 1,
+                    PurchaseType = 0,
+                    SpawnAttachments = new List<object>(),
+                    Variants = new List<object>()
+                });
+            }
+
+            var export = new ExpansionMarketConfig
+            {
+                DisplayName = $"expansion_{Path.GetFileNameWithoutExtension(args[0])}",
+                Items = items,
+                MVersion = 4
+            };
+
+            await File.WriteAllTextAsync($"expansion_{Path.GetFileNameWithoutExtension(args[0])}.json", JsonConvert.SerializeObject(export, Formatting.Indented));
+        }
+    }
+}
